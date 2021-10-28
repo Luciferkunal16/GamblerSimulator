f1fb5e722ec9eb7c514d7e2d582e59098439bc1b
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-10-28 23:41:21 +0530

    UC2 Added: win or loose by random method

--- src/Gambler_Problem.java
+++ src/Gambler_Problem.java
@@ -3,9 +3,24 @@ package LogicalPrograms;
 public class Gambler_Problem {
 	public static final double INITIAL_STAKE = 100;
 	public static final double STAKE_BET = 100;
+	public static  int stake=0;
+	/* Win or Loose By random Function*/
+	public static void winOrLose() {
+		
+		if(Math.random()<0.5) {
+			stake++;
+			System.out.println("Win");;
+		}
+		else {
+			stake --;
+			System.out.println("Lose");
+			
+		}
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome To Gambler Simulation");
+		winOrLose();
 	
 	}
 
